1580a2153d37b6c6381104df1cb5c8c3bee91e0e
jaishana25 <96332944+jaishana25@users.noreply.github.com>
2023-04-14 12:29:06 +0530

    additional changes

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,10 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <h1>DreamHome</h1>
-      <p>Specializes in property management</p>
-      <p>By taking an intermediate role between owners who wish to rent out their furnished property and clients of DreamHome who require to rent furnished property for a fixed period.</p>
+      <div className="background-image-container"></div>
+      <h1 className="text3-container">DreamHome</h1>
+      <p className="text-container">Specializes in property management</p>
+      <p className="text2-container">By taking an intermediate role between owners who wish to rent out their furnished property and clients of DreamHome to rent furnished property for a fixed period.</p>
       <div className="buttons-container">
         <Link to="/staff" className="btn-staff">Staff Registration</Link>
         <Link to="/property" className="btn-staff">Property Registration</Link>
@@ -19,3 +20,4 @@ function Home() {
 }
 
 export default Home;
+
